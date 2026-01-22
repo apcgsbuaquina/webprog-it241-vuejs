4034fe69ff7418d3f7c51d0819001a3d3e30080d
Gian Ace Buaquiña <gsbuaquina@student.apc.edu.ph>
2026-01-22 23:29:47 +0800

    added firstsfc

---
+++ firstsfc/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Food</h1>
+  <food-item/>
+  <food-item2/>
+  <personal-profile/>
+</template>
+
+<script></script>
+
+<style></style>
---
+++ firstsfc/src/components/FoodItem2.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>{{ name }}</h2>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Bananas',
+      message: 'I like bananas'
+    };
+  }
+};
+</script>
+
+<style>
+</style>
