b6ed471b2e004673ab0cd0ab2688f9d4f99620ef
Gian Ace Buaquiña <gsbuaquina@student.apc.edu.ph>
2026-01-23 15:26:54 +0800

    Wrap food items in a div for styling

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,8 +1,9 @@
 <template>
-  <h1>Food</h1>
   <personal-profile/>
-  <food-item/>
-  <food-item2/>
+  <div class="foods">
+   <food-item/>
+   <food-item2/>
+  </div>
 </template>
 
 <script></script>
